8f41623ac51544d547a33901cbae547dd9380784
Andrew Orlov <stagereagle6@gmail.com>
2017-12-11 17:47:04 +0300

    little fix

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j;
 
+/**
+ * Created by Andrew Orlov on 10.12.2017.
+ */
 public class Calculate {
-
+	
+    /**
+     * Метод main для вывода фразы Hello World!
+	 *
+     * @param args   - параметр метода.
+     */
     public static void main (String ... args) {
         System.out.println("Hello World!");
     }
