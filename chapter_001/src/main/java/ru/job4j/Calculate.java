916ea23c0845cbdc093698e14e4d2d09e989044c
Andrew Orlov <stagereagle6@gmail.com>
2017-12-14 19:06:06 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,16 +1,30 @@
 package ru.job4j;
 
 /**
- * Created by Andrew Orlov on 10.12.2017.
+ * Calculate.
+ *
+ * @author Andrew Orlov (stagereagle6@gmail.com)
+ * @version $Id$
+ * @since 0.1
  */
 public class Calculate {
-	
+
     /**
      * Метод main для вывода фразы Hello World!
-	 
-     * @param args   - параметр метода.
+     *
+     * @param args - параметр метода.
      */
-    public static void main (String ... args) {
+    public static void main(String... args) {
         System.out.println("Hello World!");
+    }
+
+	/**
+     * Method echo.
+     *
+     * @param name Andrew Orlov.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
