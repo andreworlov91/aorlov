d32834ad36d8876278a1deb7593391c545f06358
Andrew Orlov <stagereagle6@gmail.com>
2017-12-14 19:07:45 +0300

    1.4 Junit

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,31 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author AndrewOrlov (stagereagle6@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CalculateTest {
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Andrew Orlov";
+        String expect = "Echo, echo, echo : Andrew Orlov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * //TODO add comments.
+ *
+ * @author Andrew Orlov (stagereagle6@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j;
