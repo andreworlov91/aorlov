3ad4913bfc404ac926da081ad817c6c997fb0fe6
Andrew Orlov <stagereagle6@gmail.com>
2017-12-27 02:44:59 +0300

    3.1. Глупый бот.

---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Andrew Orlov (stagereagle6@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBotTest {
+    private static DummyBot instance;
+
+    private static DummyBot getInstance() {
+        if (instance == null) {
+            instance = new DummyBot();
+        }
+        return instance;
+    }
+
+    @Test
+    public void whenGreetBot() {
+        assertThat(
+                getInstance().answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    @Test
+    public void whenByuBot() {
+        assertThat(
+                getInstance().answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        assertThat(
+                getInstance().answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Спросите другой вопрос.")
+        );
+    }
+}
