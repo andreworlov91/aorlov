d97477c9fcc1113b450d4784485b967239b42182
Andrew Orlov <stagereagle6@gmail.com>
2017-12-28 21:28:15 +0300

    4.2. Создать программу вычисляющую факториал.

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,4 +1,25 @@
 package ru.job4j.loop;
 
+/**
+ * @author Andrew Orlov (stagereagle6@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Factorial {
+
+    /**
+     * Метод calc для подсчёта факториала
+     * целого числа
+     *
+     * @param n - число.
+     */
+    public int calc(int n) {
+        int factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        return factorial;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -1,4 +1,44 @@
 package ru.job4j.loop;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Andrew Orlov (stagereagle6@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class FactorialTest {
+
+    private static Factorial instance;
+    private static int fact;
+
+    private static Factorial getInstance() {
+        if (instance == null) {
+            instance = new Factorial();
+        }
+        return instance;
+    }
+
+    /**
+     * test calc
+     */
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+        fact = getInstance().calc(5);
+
+        assertThat(fact, is(120));
+    }
+
+    /**
+     * test calc
+     */
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        fact = getInstance().calc(0);
+
+        assertThat(fact, is(1));
+    }
 }
