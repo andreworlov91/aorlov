eb04ef29cbe127f040d49cb030776b121a9aa0dc
Andrew Orlov <stagereagle6@gmail.com>
2018-01-10 19:43:21 +0300

    some fix

--- chapter_001/src/main/java/ru/job4j/array/Matrix.java
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -12,16 +12,16 @@ public class Matrix {
      *
      * @param size - размер целочисленного массива.
      */
-   public int[][] multiple(int size) {
-       int[][] arr = new int[size][size];
+    public int[][] multiple(int size) {
+        int[][] arr = new int[size][size];
 
-       for (int i = 0; i < size; i++) {
-           for (int j = 0; j < size; j++) {
-               System.out.printf( "%4d", (arr[i][j] = (i+1)*(j+1)));
-           }
-           System.out.println("");
-       }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.printf("%4d", (arr[i][j] = (i + 1) * (j + 1)));
+            }
+            System.out.println("");
+        }
 
-       return arr;
-   }
+        return arr;
+    }
 }
