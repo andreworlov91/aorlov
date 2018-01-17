410921b87c732ca52d4d35772a4d069080230a13
Andrew Orlov <stagereagle6@gmail.com>
2018-01-17 21:04:19 +0300

    fix для задачи с проверкой слова в слове.

--- chapter_001/src/main/java/ru/job4j/WordInOtherWord.java
+++ chapter_001/src/main/java/ru/job4j/WordInOtherWord.java
@@ -15,18 +15,22 @@ public class WordInOtherWord {
      * @param sub    - подстрока.
      */
     public boolean contains(String origin, String sub) {
-        if (sub.length() > origin.length()) return false;
+        boolean mark = false;
 
-        char[] original = origin.toCharArray();
-        char[] substr = sub.toCharArray();
+        if (sub.length() <= origin.length()) {
+            char[] original = origin.toCharArray();
+            char[] substr = sub.toCharArray();
 
-        for (int i = 0; i <= (original.length - substr.length); i++) {
-            for (int j = 0; j < substr.length; j++) {
-                if (original[i + j] != substr[j]) break;
-                if (j == substr.length - 1) return true;
+            for (int i = 0; i <= (original.length - substr.length); i++) {
+                for (int j = 0; j < substr.length; j++) {
+                    if (original[i + j] != substr[j]) break;
+                    if (j == substr.length - 1) {
+                        mark = true;
+                    }
+                }
             }
         }
 
-        return false;
+        return mark;
     }
 }
